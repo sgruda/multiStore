7c0b08423adc040c37376cca747840c49fa795d1
Szymon Gruda <szymek.gruda@gmail.com>
2020-08-19 19:25:52 +0200

    Bugfix. Tested rest api is working.

--- src/main/java/pl/lodz/p/it/inz/sgruda/sklep/SklepApplication.java
+++ src/main/java/pl/lodz/p/it/inz/sgruda/sklep/SklepApplication.java
@@ -1,7 +1,9 @@
 package pl.lodz.p.it.inz.sgruda.sklep;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
 public class SklepApplication {
