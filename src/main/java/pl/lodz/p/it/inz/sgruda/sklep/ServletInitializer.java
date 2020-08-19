ce5e3f90aef3e687387b50cf09d93fcf497104af
Szymon Gruda <szymek.gruda@gmail.com>
2020-08-19 12:27:48 +0200

    Initial project. Using spring initializr.

---
+++ src/main/java/pl/lodz/p/it/inz/sgruda/sklep/ServletInitializer.java
@@ -0,0 +1,13 @@
+package pl.lodz.p.it.inz.sgruda.sklep;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SklepApplication.class);
+	}
+
+}
---
+++ src/main/java/pl/lodz/p/it/inz/sgruda/sklep/SklepApplication.java
@@ -0,0 +1,13 @@
+package pl.lodz.p.it.inz.sgruda.sklep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SklepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SklepApplication.class, args);
+	}
+
+}
