7f1c7ce9dc70a7fb49392a192611f9a3ffc21122
sgruda <szymon.gruda98@gmail.com>
2020-09-10 18:18:47 +0200

    Add AppProperties.java.
    Class with configuration of Auth and OAuth2

--- app/backend/src/main/java/pl/lodz/p/it/inz/sgruda/multiStore/MultiStoreApplication.java
+++ app/backend/src/main/java/pl/lodz/p/it/inz/sgruda/multiStore/MultiStoreApplication.java
@@ -2,7 +2,10 @@ package pl.lodz.p.it.inz.sgruda.multiStore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import pl.lodz.p.it.inz.sgruda.multiStore.configuration.AppProperties;
 
+@EnableConfigurationProperties(AppProperties.class)
 @SpringBootApplication
 public class MultiStoreApplication {
 
