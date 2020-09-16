57fe5922ce4e2fdcb88323800e6db1363a937cef
sgruda <szymon.gruda98@gmail.com>
2020-09-16 18:18:06 +0200

    Add some services

---
+++ app/frontend/src/services/AuthenticationService.js
@@ -0,0 +1,40 @@
+import axios from "axios";
+
+const API_URL = 'https://localhost:8181/api/auth/';
+
+
+
+class AuthService {
+  signIn(username, password) {
+    return axios
+      .post(API_URL + "signin", {
+        username,
+        password
+      })
+      .then(response => {
+        if (response.data.accessToken) {
+          localStorage.setItem("user", JSON.stringify(response.data));
+        }
+
+        return response.data;
+      });
+  }
+
+  logout() {
+    localStorage.removeItem("user");
+  }
+
+  signUp(username, email, password) {
+    return axios.post(API_URL + "signup", {
+      username,
+      email,
+      password
+    });
+  }
+
+  getCurrentUser() {
+    return JSON.parse(localStorage.getItem('user'));;
+  }
+}
+
+export default new AuthService();
