48568d17ad87915126b1b510828c17cb955dd2c3
sgruda <szymon.gruda98@gmail.com>
2020-09-16 18:18:15 +0200

    Package.json etc.

--- app/frontend/src/App.js
+++ app/frontend/src/App.js
@@ -1,12 +1,80 @@
-import React, { Component } from 'react';
-import './App.css';
-import axios from 'axios'
+import React, { useState } from "react";
+import { BrowserRouter as Router, Link, Route } from "react-router-dom";
+import PrivateRoute from './routes/PrivateRoute';
+import Home from "./pages/Home";
+import Admin from "./pages/Admin";
+import { AuthContext } from "./context/AuthContext";
+import SignIn from "./pages/SignIn";
+import SignUp from './pages/SignUp';
+import Routes from './routes/Routes';
 
-const API_URL = 'https://localhost:8181/api'
-const EMPLOYEE_API_URL = `${API_URL}/db`
 
-const App = () => {
-  // ...
+
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
+
+function App(props) {
+  const classes = useStyles();
+
+  const [authTokens, setAuthTokens] = useState();
+  
+  const setTokens = (data) => {
+    localStorage.setItem("tokens", JSON.stringify(data));
+    setAuthTokens(data);
+  }
+
+  return (
+    <AuthContext.Provider value={{ authTokens, setAuthTokens: setTokens }}>
+      {/* <Router>
+        <div>
+        <ul>
+          <li>
+            <Link to="/">Home Pagee</Link>
+          </li>
+          <li>
+            <Link to="/admin">Admin Pagee</Link>
+          </li>
+        </ul>
+          <Route exact path="/" component={Home} />
+          <Route path="/signin" component={SignIn} />
+          <Route path="/signup" component={SignUp} />
+          <PrivateRoute path="/admin" component={Admin} />
+        </div>
+      </Router> */}
+       <AppBar position="static">
+          <Toolbar>
+            <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" className={classes.title}>
+              EMPIK
+            </Typography>
+            <Button href="/signin" color="inherit">
+              SignIn
+            </Button>
+          </Toolbar>
+      </AppBar>
+      <Routes />
+    </AuthContext.Provider>
+  );
 }
 
 export default App;
--- app/frontend/src/index.js
+++ app/frontend/src/index.js
@@ -1,22 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import { BrowserRouter as Router } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
 
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// );
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+
 ReactDOM.render(
-  <Router>
-      <App />
-  </Router>, 
-  document.getElementById('root')
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById("root")
 );
+
+
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
