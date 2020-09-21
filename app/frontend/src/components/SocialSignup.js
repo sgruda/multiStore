6d085ae1af2b7ad58696ec481f2101ddd880f1a0
sgruda <szymon.gruda98@gmail.com>
2020-09-21 20:22:10 +0200

    Refactoring SocialSignup

--- app/frontend/src/components/SocialSignup.js
+++ app/frontend/src/components/SocialSignup.js
@@ -1,13 +1,7 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
 import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
-import Box from '@material-ui/core/Box';
 
 import { GOOGLE_AUTH_URL, FACEBOOK_AUTH_URL } from '../config/config';
 
