904895f789f37161a0066afb08e5005c5feee967
sgruda <szymon.gruda98@gmail.com>
2020-09-22 19:56:29 +0200

    Comment OAuth2RedicrectHandler.js

--- app/frontend/src/services/OAuth2RedirectHandler.js
+++ app/frontend/src/services/OAuth2RedirectHandler.js
@@ -3,29 +3,33 @@ import { ACCESS_TOKEN } from '../config/config';
 import { Redirect } from 'react-router-dom'
 import { useAuth } from "../context/AuthContext";
 import { useHistory } from "react-router-dom";
+import Admin from '../pages/Admin';
 
 function OAuth2RedirectHandler() {
     const getUrlParameter  = (name) => {
-        name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
-        var regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
-
-        var results = regex.exec(this.props.location.search);
-        return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
+        
+        // name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
+        // console.error("WTF + name " + name)
+        // var regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
+        // console.error("WTF + regex " + regex)
+        // var results = regex.exec(this.props.location.search);
+        // return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
     }
     
     const {setUserIsAuthenticated} = useAuth();
     const history = useHistory();
-    const token = getUrlParameter('token');
-    const error = getUrlParameter('error');  
+    // const token = getUrlParameter('token');
+    // const error = getUrlParameter('error');  
 
-    if(token) {
-        localStorage.setItem(ACCESS_TOKEN, token);
-        history.push("/admin");
+    // if(token) {
+        localStorage.setItem(ACCESS_TOKEN, "token");
+        // history.push("/admin");
         setUserIsAuthenticated(true);
-    } else {
-        history.push("/login");
-        setUserIsAuthenticated(false);
-    }
+        return Admin;
+    // } else {
+    //     history.push("/login");
+    //     setUserIsAuthenticated(false);
+    // }
 }
 
 export default OAuth2RedirectHandler;
