aa02c62ff3fa05d464137ebbe1e021752950a319
sgruda <szymon.gruda98@gmail.com>
2020-09-22 14:00:40 +0200

    Change toString() for name() in OAuth2UserInfoFactory.

--- app/backend/src/main/java/pl/lodz/p/it/inz/sgruda/multiStore/security/oauth2/user/OAuth2UserInfoFactory.java
+++ app/backend/src/main/java/pl/lodz/p/it/inz/sgruda/multiStore/security/oauth2/user/OAuth2UserInfoFactory.java
@@ -10,7 +10,7 @@ public class OAuth2UserInfoFactory {
     public static OAuth2UserInfo getOAuth2UserInfo(String registrationId, Map<String, Object> attributes) {
         if(registrationId.equalsIgnoreCase(AuthProvider.GOOGLE.name())) {
             return new GoogleOAuth2UserInfo(attributes);
-        } else if (registrationId.equalsIgnoreCase(AuthProvider.FACEBOOK.toString())) {
+        } else if (registrationId.equalsIgnoreCase(AuthProvider.FACEBOOK.name())) {
             return new FacebookOAuth2UserInfo(attributes);
         } else {
             throw new OAuth2AuthenticationProcessingException("Sorry! Login with " + registrationId + " is not supported yet.");
