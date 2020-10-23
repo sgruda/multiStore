20a7c514997994759ec5ce2038102a4c0d731b2c
sgruda <szymon.gruda98@gmail.com>
2020-10-23 21:19:09 +0200

    [Backend] Fix docker (configuration properties with connectin pool)

--- app/backend/src/main/java/pl/lodz/p/it/inz/sgruda/multiStore/configuration/persistence/PersistenceMokConfig.java
+++ app/backend/src/main/java/pl/lodz/p/it/inz/sgruda/multiStore/configuration/persistence/PersistenceMokConfig.java
@@ -2,6 +2,7 @@ package pl.lodz.p.it.inz.sgruda.multiStore.configuration.persistence;
 
 import com.zaxxer.hikari.HikariDataSource;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
@@ -28,12 +29,25 @@ import javax.sql.DataSource;
         transactionManagerRef= "mokTransactionManager"
 )
 public class PersistenceMokConfig {
+    @Value("${spring.datasource.mok.url}")
+    private String url;
+    @Value("${spring.datasource.mok.username}")
+    private String username;
+    @Value("${spring.datasource.mok.password}")
+    private String password;
+    @Value("${spring.datasource.mok.driverClassName}")
+    private String driver;
 
     @Bean
     @Primary
-    @ConfigurationProperties("spring.datasource.mok")
+//    @ConfigurationProperties("spring.datasource.mok")
     public DataSourceProperties mokDataSourceProperties() {
-        return new DataSourceProperties();
+        DataSourceProperties dataSourceProperties = new DataSourceProperties();
+        dataSourceProperties.setUrl(url);
+        dataSourceProperties.setUsername(username);
+        dataSourceProperties.setPassword(password);
+        dataSourceProperties.setDriverClassName(driver);
+        return dataSourceProperties;
     }
 
     @Bean
@@ -43,7 +57,6 @@ public class PersistenceMokConfig {
         return mokDataSourceProperties().initializeDataSourceBuilder()
                 .type(HikariDataSource.class).build();
     }
-
     @Primary
     @Bean(name = "mokManagerFactory")
     public LocalContainerEntityManagerFactoryBean mokManagerFactory(EntityManagerFactoryBuilder builder) {
