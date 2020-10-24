1261d7afb266d7384e00f0ec2dbdb7aad183e054
sgruda <szymon.gruda98@gmail.com>
2020-10-24 13:55:05 +0200

    [BACKEND] Add test method and change sensitive values for environmental variables

--- app/backend/src/test/java/pl/lodz/p/it/inz/sgruda/multiStore/MultiStoreApplicationTests.java
+++ app/backend/src/test/java/pl/lodz/p/it/inz/sgruda/multiStore/MultiStoreApplicationTests.java
@@ -1,6 +1,7 @@
 package pl.lodz.p.it.inz.sgruda.multiStore;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
@@ -8,6 +9,8 @@ class MultiStoreApplicationTests {
 
 	@Test
 	void contextLoads() {
+		Assertions.assertEquals(4, 2+2);
 	}
+
 
 }
