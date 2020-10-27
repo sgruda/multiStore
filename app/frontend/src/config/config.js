ffe13399537765de4d1adbe1d38d2c58f9a79210
sgruda <szymon.gruda98@gmail.com>
2020-10-27 18:55:44 +0100

    test

--- app/frontend/src/config/config.js
+++ app/frontend/src/config/config.js
@@ -1,4 +1,4 @@
-// export const API_BASE_URL = 'http://localhost:8080';
+
 export const API_BASE_URL = 'https://multistore-app-yka4rjl6za-lz.a.run.app';
 
 export const ACCESS_TOKEN = 'accessToken';
