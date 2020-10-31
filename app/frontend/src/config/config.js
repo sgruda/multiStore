9827bc867c0da9e74f35e3b4639ce9222ded11b9
sgruda <szymon.gruda98@gmail.com>
2020-10-31 21:53:07 +0100

    [DEVOPS] Divide into 3 services, placed in docker containters.

--- app/frontend/src/config/config.js
+++ app/frontend/src/config/config.js
@@ -3,10 +3,8 @@ export const API_BASE_URL = "http://localhost:8080";
 
 export const ACCESS_TOKEN = 'accessToken';
 
-// export const OAUTH2_REDIRECT_URI = 'http://localhost:8080/oauth2/redirect';
-// export const OAUTH2_REDIRECT_URI = process.env.REACT_APP_API_BASE_URL + '/oauth2/redirect';
-// export const OAUTH2_REDIRECT_URI = 'https://multistore-app-yka4rjl6za-lz.a.run.app/oauth2/redirect';
-export const OAUTH2_REDIRECT_URI = API_BASE_URL + '/oauth2/redirect';
+export const REACT_APP_URL = 'http://localhost:5000';
+export const OAUTH2_REDIRECT_URI = REACT_APP_URL + '/oauth2/redirect';
 
 export const GOOGLE_AUTH_URL = API_BASE_URL + '/oauth2/authorize/google?redirect_uri=' + OAUTH2_REDIRECT_URI;
 export const FACEBOOK_AUTH_URL = API_BASE_URL + '/oauth2/authorize/facebook?redirect_uri=' + OAUTH2_REDIRECT_URI;
