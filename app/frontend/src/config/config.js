8ed74cf0802f7b49e00a325e7a649a6c136113f7
sgruda <szymon.gruda98@gmail.com>
2020-11-01 20:28:29 +0100

    [DEVOPS] Travis update

--- app/frontend/src/config/config.js
+++ app/frontend/src/config/config.js
@@ -1,9 +1,14 @@
-export const API_BASE_URL = "http://localhost:8080";
+// export const API_BASE_URL = "http://localhost:8080";
 // export const API_BASE_URL = 'https://multistore-app-yka4rjl6za-lz.a.run.app';
+// export const API_BASE_URL = process.env.API_BASE_URL;
+export const API_BASE_URL = 'https://multistore-backend-yka4rjl6za-ey.a.run.app';
 
 export const ACCESS_TOKEN = 'accessToken';
 
-export const REACT_APP_URL = 'http://localhost:5000';
+// export const REACT_APP_URL = 'http://localhost:5000';
+// export const REACT_APP_URL = process.env.REACT_APP_URL;
+export const REACT_APP_URL =  'https://multistore-app-frontend-yka4rjl6za-ey.a.run.app';
+
 export const OAUTH2_REDIRECT_URI = REACT_APP_URL + '/oauth2/redirect';
 
 export const GOOGLE_AUTH_URL = API_BASE_URL + '/oauth2/authorize/google?redirect_uri=' + OAUTH2_REDIRECT_URI;
