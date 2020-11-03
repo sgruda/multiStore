4d638e075c79e61528307a11d3e341f15d9b5b20
sgruda <szymon.gruda98@gmail.com>
2020-11-03 22:16:19 +0100

    [BACKEND] Change BCrypt to sha256 in HashGenerator

--- app/backend/src/main/java/pl/lodz/p/it/inz/sgruda/multiStore/utils/HashGenerator.java
+++ app/backend/src/main/java/pl/lodz/p/it/inz/sgruda/multiStore/utils/HashGenerator.java
@@ -1,28 +1,50 @@
 package pl.lodz.p.it.inz.sgruda.multiStore.utils;
 
 import lombok.NoArgsConstructor;
-import org.springframework.security.crypto.bcrypt.BCrypt;
+import lombok.extern.java.Log;
+import java.nio.charset.StandardCharsets;
+import java.security.*;
 
+@Log
 @NoArgsConstructor
 public class HashGenerator {
-//    @Value("${app.auth.bcrypt.secret.salt}")
-//    private String saltBCrypt;
 
     public String hash(String toHash) {
-        return  BCrypt.hashpw(toHash, BCrypt.gensalt());
+        return sha256(toHash);
     }
     public String hash(long toHash) {
-        return  BCrypt.hashpw(String.valueOf(toHash), BCrypt.gensalt());
+        return sha256(String.valueOf(toHash));
     }
     public String sign(String idHash, String param, long version) {
-        return BCrypt.hashpw(idHash + param + version, BCrypt.gensalt());
+        return sha256(idHash + param + version);
     }
 
 
     public boolean checkHash(String text, String hashText) {
-        return BCrypt.checkpw(text, hashText);
+        return sha256(text).equals(hashText) ? true : false;
     }
     public boolean checkSignature(String signature, String idHash, String param, long version) {
-        return BCrypt.checkpw(signature, idHash + param + version);
+        return sha256(idHash + param + version).equals(signature) ? true : false;
+    }
+    private String sha256(String toHash) {
+        MessageDigest digest = null;
+        try {
+            digest = MessageDigest.getInstance("SHA-256");
+        } catch (NoSuchAlgorithmException e) {
+            log.severe(e.getClass().toString() + " " + e.getMessage());
+        }
+        byte[] hash = new byte[0];
+        if (digest != null) {
+            hash = digest.digest(toHash.getBytes(StandardCharsets.UTF_8));
+        }
+        StringBuilder hexString = new StringBuilder();
+        for (byte b : hash) {
+            String hex = Integer.toHexString(0xff & b);
+            if (hex.length() == 1) {
+                hexString.append('0');
+            }
+            hexString.append(hex);
+        }
+        return hexString.toString();
     }
 }
--- app/backend/src/test/java/pl/lodz/p/it/inz/sgruda/multiStore/HashGeneratorTest.java
+++ app/backend/src/test/java/pl/lodz/p/it/inz/sgruda/multiStore/HashGeneratorTest.java
@@ -1,6 +1,5 @@
 package pl.lodz.p.it.inz.sgruda.multiStore;
 
-import ch.qos.logback.core.net.AbstractSSLSocketAppender;
 import lombok.extern.java.Log;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -21,7 +20,7 @@ public class HashGeneratorTest {
 
         long toHash2 = 7;
         String idHash2 = hashGenerator.hash(toHash2);
-        String param2 = param+"a";//"kazimierz.wielki@gmail.com";
+        String param2 = "kazimierz.wielki@gmail.com";
         long version2 = 2;
         String signature2 = hashGenerator.sign(idHash2, param2, version2);
 
@@ -29,11 +28,17 @@ public class HashGeneratorTest {
         Assertions.assertEquals(true, hashGenerator.checkHash(String.valueOf(toHash), idHash));
         Assertions.assertEquals(false, hashGenerator.checkHash(String.valueOf(toHash2), idHash));
         Assertions.assertEquals(false, hashGenerator.checkHash(String.valueOf(toHash), idHash2));
+        Assertions.assertEquals(false, hashGenerator.checkHash(String.valueOf(toHash), idHash + "a"));
+        Assertions.assertEquals(false, hashGenerator.checkHash(String.valueOf(toHash + 1), idHash));
 
         Assertions.assertEquals(true, hashGenerator.checkSignature(signature, idHash, param, version));
         Assertions.assertEquals(false, hashGenerator.checkSignature(signature2, idHash, param, version));
         Assertions.assertEquals(false, hashGenerator.checkSignature(signature, idHash2, param, version));
         Assertions.assertEquals(false, hashGenerator.checkSignature(signature, idHash, param2, version));
         Assertions.assertEquals(false, hashGenerator.checkSignature(signature, idHash, param, version2));
+        Assertions.assertEquals(false, hashGenerator.checkSignature(signature + "a", idHash, param, version));
+        Assertions.assertEquals(false, hashGenerator.checkSignature(signature, idHash + "a", param, version));
+        Assertions.assertEquals(false, hashGenerator.checkSignature(signature, idHash, param + "a", version));
+        Assertions.assertEquals(false, hashGenerator.checkSignature(signature, idHash, param, version + 1));
     }
 }
