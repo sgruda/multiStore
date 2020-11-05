b2c78bf14e5a7c4859556a3999ede2662384bd6d
sgruda <szymon.gruda98@gmail.com>
2020-11-05 16:20:36 +0100

    [BACKEND] add cors (delete localhost:5000)

--- app/backend/src/main/java/pl/lodz/p/it/inz/sgruda/multiStore/configuration/WebConfig.java
+++ app/backend/src/main/java/pl/lodz/p/it/inz/sgruda/multiStore/configuration/WebConfig.java
@@ -16,7 +16,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:3000")
-                .allowedOrigins("http://localhost:5000")
+//                .allowedOrigins("http://localhost:5000")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
