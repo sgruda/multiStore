f45269a6aa593b8d8cde70d3035beb95294ea58e
sgruda <szymon.gruda98@gmail.com>
2020-11-06 21:46:08 +0100

    [FRONTEND] Add new queries to AccountService

--- app/frontend/src/services/AccountService.js
+++ app/frontend/src/services/AccountService.js
@@ -4,8 +4,20 @@ import {API_URL_ACCOUNTS, API_URL_SINGLE_ACCOUNT} from '../config/config';
  
 class AccountService { 
 
-  getAccounts() { 
-    return axios.get(API_URL_ACCOUNTS, { headers: AuthorizationHeader() }); 
+  // getAccounts() { 
+  //   return axios.get(API_URL_ACCOUNTS, { headers: AuthorizationHeader() }); 
+  // } 
+  getAccounts(textToSearch, page, size, sort, active) { 
+    const API_URL_ACCOUNTS_PAGINATION = API_URL_ACCOUNTS + '?page=' + page + '&size=' + size + '&sort=' + sort;
+    if(textToSearch == null && active == null)
+      return axios.get(API_URL_ACCOUNTS_PAGINATION, { headers: AuthorizationHeader() }); 
+    else if(textToSearch != null && active == null)
+      return axios.get(API_URL_ACCOUNTS_PAGINATION + '&textToSearch=' + textToSearch, { headers: AuthorizationHeader() }); 
+    else if(textToSearch == null && active != null)
+      return axios.get(API_URL_ACCOUNTS_PAGINATION + '&active=' + active, { headers: AuthorizationHeader() });
+    else if(textToSearch != null && active != null)
+      return axios.get(API_URL_ACCOUNTS_PAGINATION + '&active=' + active + '&textToSearch=' + textToSearch, { headers: AuthorizationHeader() });
+
   } 
  
   getSingleAccount(email) { 
