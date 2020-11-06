46b8ec115203a2c496efb1a0fabeadb06fb44943
sgruda <szymon.gruda98@gmail.com>
2020-11-06 19:22:00 +0100

    [FRONTEND] little fix

--- app/frontend/src/pages/AccountList.js
+++ app/frontend/src/pages/AccountList.js
@@ -4,19 +4,20 @@ import { DataGrid } from '@material-ui/data-grid';
 import { useAuth } from "../context/AuthContext";
 import { useFields } from '../hooks/FieldHook';
 
+import ClearIcon from '@material-ui/icons/Clear';
+import DoneIcon from '@material-ui/icons/Done';
 
 import AccountService from '../services/AccountService';
 
 
     const columns = [
-        { field: 'idHash', headerName: 'ID', width: 70 },
-        // { field: 'idHash', hide: true, },
+        { field: 'idHash', hide: true, },
         { field: 'firstName', headerName: 'First name', width: 130 },
         { field: 'lastName', headerName: 'Last name', width: 130 },
-        { field: 'email', headerName: 'Email', type: 'email', width: 200, },
-        { field: 'active', headerName: 'Active', type: 'boolean', width: 80,},
+        { field: 'email', headerName: 'Email', width: 200, },
+        { field: 'active', headerName: 'Active', width: 90,},
         { field: 'roles', headerName: 'Roles', width: 300, },
-        // { field: 'fullName', headerName: 'Full name', sortable: false, width: 160,
+        // { field: 'fullName', headerName: 'Full name', sortable: false, width: 160, },
         // description: 'This column has a value getter and is not sortable.', 
         // valueGetter: (params) =>
         //     `${params.getValue('firstName') || ''} ${params.getValue('lastName') || ''}`,
@@ -30,13 +31,26 @@ import AccountService from '../services/AccountService';
 function AccountList() {
     const [loadingData, setLoadingData] = useState(true);
     const [accounts, setAccounts] = useState([]);
+    const [page, setPage] = React.useState(1);
     const paginationInfo = [{
         currentPage: 0,
         totalItems: 0,
         totalPages: 0
     }];
+    const requestParams = [{
+        textToSearch: null,
+        page: 0,
+        size: 5,
+        sort: [],
+        active: null
+    }];
+
+    const handlePageChange = (params) => {
+        setPage(params.page);
+    };
 
     async function getAccounts() {
+        // setLoadingData(true);
         await AccountService.getAccounts()
         .then(response => {
             if (response.status === 200) { 
@@ -51,11 +65,12 @@ function AccountList() {
                     };
                 });
                 setAccounts(accounts);
-                // setAccounts(response.data.accounts);
                 paginationInfo.currentPage = response.data.currentPage;
                 paginationInfo.totalItems = response.data.totalItems;
                 paginationInfo.totalPages = response.data.totalPages;
-                setLoadingData(false)
+                // setLoadingData(false);
+                console.log("loading " +loadingData)
+                console.log("data " + response.data.totalItems)
             }
         },
             (error) => {
@@ -67,16 +82,27 @@ function AccountList() {
     }
 
     useEffect(() => {
+        console.log("loading " +loadingData)
         if (loadingData) {
+            setLoadingData(false);
             getAccounts();
         }
-    }, []);
+    }, [page, accounts]);
 
   return (
-    <div style={{ height: 400, width: '100%' }}>
-      <DataGrid rows={accounts} columns={columns} key="idHash" pageSize={5} checkboxSelection />
+    <div style={{ height: 400, width: '90%' }}>
+      <DataGrid rows={accounts} 
+                columns={columns}  
+                pagination
+                pageSize={requestParams.size} 
+                rowsPerPageOptions={[5,10,100]}
+                paginationMode='server'
+                sortingMode='server'
+                onPageChange={handlePageChange}
+                loading={loadingData}
+                rowCount={paginationInfo.totalItems}
+        />
     </div>
-    // <div><Button onClick={getAccounts}> reafsgx</Button></div>
   );
 }
 
