c215a00624db761a05bd41325d1aa3f14286f88b
sgruda <szymon.gruda98@gmail.com>
2020-11-14 19:02:07 +0100

    [FRONTEND] Add textArea with validation to searching

--- app/frontend/src/components/accounts/AccountsTableToolbar.js
+++ app/frontend/src/components/accounts/AccountsTableToolbar.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { useForm } from "react-hook-form";
 
 import clsx from 'clsx';
 import { makeStyles } from '@material-ui/core/styles';
@@ -12,9 +13,12 @@ import Collapse from '@material-ui/core/Collapse';
 import { Grid } from "@material-ui/core";
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
-
+import KeyboardArrowUpIcon from '@material-ui/icons/KeyboardArrowUp';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 
 import AccountDetails from '../accounts/AccountDetails';
+import { useFields } from '../../hooks/FieldHook';
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -49,16 +53,27 @@ function AccountsTableToolbar({selectedAccountMail, selectedAccountName}) {
     const classes = useStyles();
     const aboutAccount = 'Name: ' + selectedAccountName; 
     const [expandedDetails, setExpandedDetails] = useState(false);
-
+    const [expandedSearching, setExpandedSearching] = useState(false);
+    // const [textToSearch, setTextToSearch] = useState("");
+    const [fields, setFields] = useFields({
+      textToSearch: ""
+    });
+    const { register, handleSubmit, errors } = useForm({mode: "onSubmit"}); 
 
 
     const handleExpandDetails = () => {
       setExpandedDetails(!expandedDetails);
     }
+    const handleExpandedSearching = () => {
+      setExpandedSearching(!expandedSearching);
+    }
+    const handleSearch = () => {
 
+    }
 
     useEffect(() => {
       setExpandedDetails(false);
+      setExpandedSearching(false);
     }, [selectedAccountMail]);
 
     return (
@@ -90,7 +105,37 @@ function AccountsTableToolbar({selectedAccountMail, selectedAccountName}) {
                 </Collapse>
                 </Grid>
               </Grid>
-            ) : (<div/>) }
+            ) : (
+              <Grid container justify="center">
+                <Collapse in={expandedSearching} timeout="auto" unmountOnExit>
+                  <form noValidate onSubmit={handleSubmit(handleSearch)}>
+                    <TextField
+                      value={ fields.textToSearch }
+                      onChange={ setFields }
+                      name="textToSearch"
+                      required
+                      fullWidth
+                      id="textToSearch"
+                      label="Search"
+
+                      inputRef={register({ required: true,  pattern: /[a-zA-Z0-9!@#$%^*]+/ })}
+                      error={errors.textToSearch ? true : false}
+                      helperText={errors.textToSearch ? "Incorrect entry." : ""}
+                    />
+                  <Tooltip title="Search">
+                    <IconButton aria-label="Search" type="submit">
+                        <SearchIcon />
+                    </IconButton>
+                    {/* <Button
+                  type="submit"
+                >
+                  Search
+                </Button> */}
+                  </Tooltip>
+                  </form>
+                </Collapse>
+              </Grid>
+            )}
               {selectedAccountMail !== '' ? (
               <Grid  alignItems="center">
                 <Tooltip title="Details">
@@ -106,11 +151,13 @@ function AccountsTableToolbar({selectedAccountMail, selectedAccountName}) {
                 </Tooltip>
               </Grid>
                ) : (
-              <Tooltip title="Search">
-              <IconButton aria-label="Search">
-                  <SearchIcon />
-              </IconButton>
-              </Tooltip>
+              <Grid  alignItems="center">
+                <Tooltip title="Search">
+                <IconButton aria-label="Expand Search" onClick={handleExpandedSearching}>
+                    {!expandedSearching ? <SearchIcon /> : <KeyboardArrowUpIcon/>}
+                </IconButton>
+                </Tooltip>
+              </Grid>
               )}
             </Grid>
         </Toolbar>
