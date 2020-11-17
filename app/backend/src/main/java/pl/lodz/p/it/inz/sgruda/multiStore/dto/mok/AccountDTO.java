a30cc8b4cc99b3fc3ce6914b94a9547b9c3f197e
sgruda <szymon.gruda98@gmail.com>
2020-11-17 17:10:42 +0100

    [BACKEND] Add some validation to dto

--- app/backend/src/main/java/pl/lodz/p/it/inz/sgruda/multiStore/dto/mok/AccountDTO.java
+++ app/backend/src/main/java/pl/lodz/p/it/inz/sgruda/multiStore/dto/mok/AccountDTO.java
@@ -4,9 +4,12 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
+import org.springframework.validation.annotation.Validated;
 import pl.lodz.p.it.inz.sgruda.multiStore.utils.enums.RoleName;
 import pl.lodz.p.it.inz.sgruda.multiStore.utils.interfaces.SignatureVerifiability;
 
+import javax.validation.Valid;
+import javax.validation.constraints.*;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
@@ -17,12 +20,27 @@ import java.util.Set;
 @AllArgsConstructor
 public @Data class AccountDTO implements SignatureVerifiability {
     private String idHash;
+
+    @NotNull(message = "{validation.notnull}")
+    @Size(min = 1, max = 32, message = "{validation.size}")
+    @Pattern(regexp = "^[A-ZĄĆĘŁŃÓŚŹŻ]{1}[a-zA-ZąćęłńóśźżĄĆĘŁŃÓŚŹŻ]+", message = "{validation.pattern}")
     private String firstName;
+
+    @NotNull(message = "{validation.notnull}")
+    @Size(min = 1, max = 32, message = "{validation.size}")
+    @Pattern(regexp = "^[A-ZĄĆĘŁŃÓŚŹŻ]{1}[a-zA-ZąćęłńóśźżĄĆĘŁŃÓŚŹŻ]+", message = "{validation.pattern}")
     private String lastName;
+
+    @NotNull(message = "{validation.notnull}")
+    @Email(message = "{validation.email}")
+    @Size(min = 1, max = 32, message = "{validation.size}")
     private String email;
     private Set<String> roles = new HashSet<>();
     private boolean active;
     private String authProvider;
+
+    @NotNull
+    @Valid
     private AuthenticationDataDTO authenticationDataDTO;
     private long version;
     private String signature;
--- app/backend/src/main/java/pl/lodz/p/it/inz/sgruda/multiStore/dto/mok/AuthenticationDataDTO.java
+++ app/backend/src/main/java/pl/lodz/p/it/inz/sgruda/multiStore/dto/mok/AuthenticationDataDTO.java
@@ -3,6 +3,9 @@ package pl.lodz.p.it.inz.sgruda.multiStore.dto.mok;
 import lombok.*;
 import pl.lodz.p.it.inz.sgruda.multiStore.utils.interfaces.SignatureVerifiability;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 import java.util.Arrays;
 import java.util.List;
 
@@ -12,7 +15,14 @@ import java.util.List;
 public @Data class AuthenticationDataDTO implements SignatureVerifiability {
 //    private String veryficationToken;.
     private String idHash;
+
+    @NotNull(message = "{validation.notnull}")
+    @Size(min = 1, max = 32, message = "{validation.size}")
+    @Pattern(regexp = "[a-zA-Z0-9!@#$%^*]+", message = "{validation.pattern}")
     private String username;
+
+    @NotNull(message = "{validation.notnull}")
+    @Pattern(regexp = "(?=^.{8,}$)((?=.*\\d)|(?=.*\\W+))(?![.\\n])(?=.*[A-Z])(?=.*[a-z]).*$", message = "{validation.pattern}")
     private String password;
     private boolean emailVerified;
     private ForgotPasswordTokenDTO forgotPasswordTokenDTO;
