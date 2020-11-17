6630b4d6ddf2e1866a652e49dc530a5752fbcebe
sgruda <szymon.gruda98@gmail.com>
2020-11-17 17:11:09 +0100

    [BACKEND] Add changing password by owner.

---
+++ app/backend/src/main/java/pl/lodz/p/it/inz/sgruda/multiStore/mok/endpoints/AccountOwnerOperationsEndpoint.java
@@ -0,0 +1,63 @@
+package pl.lodz.p.it.inz.sgruda.multiStore.mok.endpoints;
+
+import lombok.extern.java.Log;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
+import pl.lodz.p.it.inz.sgruda.multiStore.dto.mappers.mok.AccountMapper;
+import pl.lodz.p.it.inz.sgruda.multiStore.dto.mok.AccountDTO;
+import pl.lodz.p.it.inz.sgruda.multiStore.entities.AccountEntity;
+import pl.lodz.p.it.inz.sgruda.multiStore.exceptions.dto.DTOSignatureException;
+import pl.lodz.p.it.inz.sgruda.multiStore.exceptions.dto.DTOVersionException;
+import pl.lodz.p.it.inz.sgruda.multiStore.exceptions.mok.AccountNotExistsException;
+import pl.lodz.p.it.inz.sgruda.multiStore.mok.services.interfaces.OwnPasswordChangeService;
+import pl.lodz.p.it.inz.sgruda.multiStore.responses.ApiResponse;
+import pl.lodz.p.it.inz.sgruda.multiStore.utils.components.CheckerAccountDTO;
+
+import javax.validation.Valid;
+
+@Log
+@RestController
+@Transactional(
+        propagation = Propagation.NEVER
+)
+@RequestMapping("/api/accounts/me")
+public class AccountOwnerOperationsEndpoint {
+    private OwnPasswordChangeService ownPasswordChangeService;
+    private CheckerAccountDTO checkerAccountDTO;
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public AccountOwnerOperationsEndpoint(OwnPasswordChangeService ownPasswordChangeService, CheckerAccountDTO checkerAccountDTO, PasswordEncoder passwordEncoder) {
+        this.ownPasswordChangeService = ownPasswordChangeService;
+        this.checkerAccountDTO = checkerAccountDTO;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @PutMapping("/change-password")
+    public ResponseEntity<?> changePassword(@Valid @RequestBody AccountDTO accountDTO) {
+        AccountEntity accountEntity;
+        try {
+            log.severe("Hello");
+            accountEntity = ownPasswordChangeService.getAccountByEmail(accountDTO.getEmail());
+            log.severe("wersja entity " + accountEntity.getVersion());
+        } catch (AccountNotExistsException e) {
+            log.severe("Error: " + e);
+            return new ResponseEntity(new ApiResponse(false, e.getMessage()),
+                    HttpStatus.NOT_FOUND);
+        }
+        try {
+            checkerAccountDTO.checkIntegrity(accountEntity, accountDTO);
+            ownPasswordChangeService.changePassword(accountEntity, passwordEncoder.encode(accountDTO.getAuthenticationDataDTO().getPassword()));
+        } catch (DTOSignatureException | DTOVersionException e) {
+            log.severe("Error: " + e);
+            return new ResponseEntity(new ApiResponse(false, e.getMessage()),
+                    HttpStatus.BAD_REQUEST);
+        }
+        return ResponseEntity.ok(new ApiResponse(true, "account.password.change.correctly."));
+    }
+}
---
+++ app/backend/src/main/java/pl/lodz/p/it/inz/sgruda/multiStore/mok/services/implementation/OwnPasswordChangeServiceImpl.java
@@ -0,0 +1,43 @@
+package pl.lodz.p.it.inz.sgruda.multiStore.mok.services.implementation;
+
+import lombok.extern.java.Log;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+import pl.lodz.p.it.inz.sgruda.multiStore.entities.AccountEntity;
+import pl.lodz.p.it.inz.sgruda.multiStore.exceptions.AppBaseException;
+import pl.lodz.p.it.inz.sgruda.multiStore.exceptions.mok.AccountNotExistsException;
+import pl.lodz.p.it.inz.sgruda.multiStore.mok.repositories.AccountRepository;
+import pl.lodz.p.it.inz.sgruda.multiStore.mok.services.interfaces.OwnPasswordChangeService;
+
+@Log
+@Service
+@Transactional(
+        isolation = Isolation.READ_COMMITTED,
+        propagation = Propagation.REQUIRES_NEW,
+        timeout = 5
+)
+public class OwnPasswordChangeServiceImpl implements OwnPasswordChangeService {
+    private AccountRepository accountRepository;
+
+    @Autowired
+    public OwnPasswordChangeServiceImpl(AccountRepository accountRepository) {
+        this.accountRepository = accountRepository;
+    }
+
+    @Override
+    @PreAuthorize("hasRole('ROLE_CLIENT') or hasRole('ROLE_EMPLOYEE') or  hasRole('ROLE_ADMIN')")
+    public void changePassword(AccountEntity accountEntity, String newEncodedPassword) {
+        accountEntity.setPassword(newEncodedPassword);
+    }
+
+    @Override
+    @PreAuthorize("hasRole('ROLE_CLIENT') or hasRole('ROLE_EMPLOYEE') or  hasRole('ROLE_ADMIN')")
+    public AccountEntity getAccountByEmail(String mail) throws AccountNotExistsException {
+        return accountRepository.findByEmail(mail)
+                .orElseThrow(() -> new AccountNotExistsException());
+    }
+}
