963c1bda514a0699f46be6af2619b2d90172a3b4
sgruda <szymon.gruda98@gmail.com>
2020-11-19 13:06:48 +0100

    [BACKEND] Add editing account by admin.

---
+++ app/backend/src/main/java/pl/lodz/p/it/inz/sgruda/multiStore/mok/services/implementation/AccountEditServiceImpl.java
@@ -0,0 +1,47 @@
+package pl.lodz.p.it.inz.sgruda.multiStore.mok.services.implementation;
+
+import lombok.extern.java.Log;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+import pl.lodz.p.it.inz.sgruda.multiStore.entities.AccountEntity;
+import pl.lodz.p.it.inz.sgruda.multiStore.exceptions.mok.AccountNotExistsException;
+import pl.lodz.p.it.inz.sgruda.multiStore.exceptions.mok.OperationDisabledForAccountException;
+import pl.lodz.p.it.inz.sgruda.multiStore.mok.repositories.AccountRepository;
+import pl.lodz.p.it.inz.sgruda.multiStore.mok.services.interfaces.AccountEditService;
+import pl.lodz.p.it.inz.sgruda.multiStore.utils.enums.AuthProvider;
+
+@Log
+@Service
+@Transactional(
+        isolation = Isolation.READ_COMMITTED,
+        propagation = Propagation.REQUIRES_NEW,
+        timeout = 5
+)
+public class AccountEditServiceImpl implements AccountEditService {
+    private AccountRepository accountRepository;
+
+    @Autowired
+    public AccountEditServiceImpl(AccountRepository accountRepository) {
+        this.accountRepository = accountRepository;
+    }
+
+    @Override
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    public void editAccount(AccountEntity accountEntity) throws OperationDisabledForAccountException {
+        if(accountEntity.getProvider() == AuthProvider.system)
+            accountRepository.saveAndFlush(accountEntity);
+        else
+            throw new OperationDisabledForAccountException();
+    }
+
+    @Override
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    public AccountEntity getAccountByEmail(String mail) throws AccountNotExistsException {
+        return accountRepository.findByEmail(mail)
+                .orElseThrow(() -> new AccountNotExistsException());
+    }
+}
