bd57f8ad06eb5a118dab8174c052c1e846ecc1cc
sgruda <szymon.gruda98@gmail.com>
2020-11-20 19:27:34 +0100

    [FRONTEND] Style for confirm dialog

--- app/frontend/src/components/ConfirmDialog.js
+++ app/frontend/src/components/ConfirmDialog.js
@@ -5,14 +5,11 @@ import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
-
-const useStyles = makeStyles((theme) => ({
-    
-}));
+import { Backdrop } from '@material-ui/core';
+import DoneIcon from '@material-ui/icons/Done';
+import CloseIcon from '@material-ui/icons/Close';
 
 function ConfirmDialog({openConfirmDialog, setOpenConfirmDialog, handleConfirmAction}) {
-  const classes = useStyles();
-
   const handleOpenConfirmDialog = () => {
     setOpenConfirmDialog(!openConfirmDialog);
   }
@@ -30,16 +27,17 @@ function ConfirmDialog({openConfirmDialog, setOpenConfirmDialog, handleConfirmAc
             onClose={handleOpenConfirmDialog}
             aria-describedby="dialog-description"
         >
+            <Backdrop open={openConfirmDialog}/>
             <DialogContent>
             <DialogContentText id="dialog-description">
                 Are you sure?
             </DialogContentText>
             </DialogContent>
             <DialogActions>
-            <Button onClick={handleConfirm} color="primary" autoFocus>
+            <Button onClick={handleConfirm} color="primary" autoFocus startIcon={<DoneIcon/>}>
                 Yes
             </Button>
-            <Button onClick={handleOpenConfirmDialog} color="primary" autoFocus>
+            <Button onClick={handleOpenConfirmDialog} color="primary" autoFocus startIcon={<CloseIcon/>}>
                 No
             </Button>
             </DialogActions>
