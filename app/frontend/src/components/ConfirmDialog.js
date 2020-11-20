f314a353098705cbc2e9db8e9d56d2a5ac26368a
sgruda <szymon.gruda98@gmail.com>
2020-11-20 19:18:36 +0100

    [FRONTEND] Create component ConfirmDialog

---
+++ app/frontend/src/components/ConfirmDialog.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+
+const useStyles = makeStyles((theme) => ({
+    
+}));
+
+function ConfirmDialog({openConfirmDialog, setOpenConfirmDialog, handleConfirmAction}) {
+  const classes = useStyles();
+
+  const handleOpenConfirmDialog = () => {
+    setOpenConfirmDialog(!openConfirmDialog);
+  }
+
+  const handleConfirm = () => {
+    handleConfirmAction()
+    setOpenConfirmDialog(!openConfirmDialog);
+  }
+
+
+  return (
+    <div>
+        <Dialog
+            open={openConfirmDialog}
+            onClose={handleOpenConfirmDialog}
+            aria-describedby="dialog-description"
+        >
+            <DialogContent>
+            <DialogContentText id="dialog-description">
+                Are you sure?
+            </DialogContentText>
+            </DialogContent>
+            <DialogActions>
+            <Button onClick={handleConfirm} color="primary" autoFocus>
+                Yes
+            </Button>
+            <Button onClick={handleOpenConfirmDialog} color="primary" autoFocus>
+                No
+            </Button>
+            </DialogActions>
+        </Dialog>
+    </div >
+  );
+}
+export default ConfirmDialog;
