33fd1c6aced61673c2f0d51ea130ecdb93159767
sgruda <szymon.gruda98@gmail.com>
2020-11-23 14:14:12 +0100

    [FRONTEND] Refactoring signup. add new form

--- app/frontend/src/pages/SignUp.js
+++ app/frontend/src/pages/SignUp.js
@@ -8,6 +8,7 @@ import { onError } from '../services/exceptions/ErrorService';
 import { useFields } from '../hooks/FieldHook';
 
 import  SocialButtons from '../components/SocialButtons';
+import AddAccountForm from '../components/accounts/AddAccountForm';
 import { GOOGLE_AUTH_URL, FACEBOOK_AUTH_URL } from '../config/config';
 
 import Alert from '@material-ui/lab/Alert';
@@ -112,7 +113,6 @@ function SignUp() {
   return (
     <div>
         <div>
-
         <Container component="main" maxWidth="xs">
             <CssBaseline />
             <div className={classes.paper}>
@@ -123,115 +123,12 @@ function SignUp() {
                 Sign up
               </Typography>
               <form className={classes.form} noValidate onSubmit={handleSubmit(handleSignUp)}>
-                <Grid container spacing={2}>
-                  <Grid item xs={12} sm={6}>
-                    <TextField
-                      value={ fields.firstname }
-                      onChange={ setFields }
-                      autoComplete="fname"
-                      name="firstname"
-                      variant="outlined"
-                      required
-                      fullWidth
-                      id="firstname"
-                      label="First Name"
-
-                      inputRef={register({ required: true,  pattern: /^[A-ZĄĆĘŁŃÓŚŹŻ]{1}[a-zA-ZąćęłńóśźżĄĆĘŁŃÓŚŹŻ]+/ })}
-                      error={errors.firstname ? true : false}
-                      helperText={errors.firstname ? "Incorrect entry." : ""}
-                    />
-                  </Grid>
-                  <Grid item xs={12} sm={6}>
-                    <TextField
-                      value={ fields.lastname }
-                      onChange={ setFields }
-                      variant="outlined"
-                      required
-                      fullWidth
-                      id="lastname"
-                      label="Last Name"
-                      name="lastname"
-                      autoComplete="lname"
-
-                      inputRef={register({ required: true,  pattern: /^[A-ZĄĆĘŁŃÓŚŹŻ]{1}[a-zA-ZąćęłńóśźżĄĆĘŁŃÓŚŹŻ]+/ })}
-                      error={errors.lastname ? true : false}
-                      helperText={errors.lastname ? "Incorrect entry." : ""}
-                    />
-                  </Grid>
-                  <Grid item xs={12}>
-                    <TextField
-                      value={ fields.email }
-                      onChange={ setFields }
-                      variant="outlined"
-                      required
-                      fullWidth
-                      id="email"
-                      label="Email Address"
-                      name="email"
-                      autoComplete="email"
-
-                      inputRef={register({ required: true,  pattern: /^[_A-Za-z0-9-\+]+(\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\.[A-Za-z0-9]+)*(\.[A-Za-z]{2,})$/ })}
-                      error={errors.email ? true : false}
-                      helperText={errors.email ? "Incorrect entry." : ""}
-                    />
-                  </Grid>
-                  <Grid item xs={12}>
-                    <TextField
-                      value={ fields.username }
-                      onChange={ setFields }
-                      variant="outlined"
-                      required
-                      fullWidth
-                      id="username"
-                      label="Username"
-                      name="username"
-                      autoComplete="username"
-                      
-                      inputRef={register({ required: true,  pattern: /[a-zA-Z0-9!@#$%^*]+/ })}
-                      error={errors.username ? true : false}
-                      helperText={errors.username ? "Incorrect entry." : ""}
-                    /> 
-                  </Grid>
-                  <Grid item xs={12}>
-                    <TextField
-                      value={ fields.password }
-                      onChange={ setFields }
-                      variant="outlined"
-                      required
-                      fullWidth
-                      name="password"
-                      label="Password"
-                      type="password"
-                      id="password"
-                      autoComplete="current-password"
-
-                      inputRef={register({ required: true, minLength: 8, pattern: /(?=^.{8,}$)((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/ })}
-                      error={errors.password ? true : false}
-                      helperText={errors.password ? "Password is required (must have 8 digits and...)" : ""}
-                  />
-                  </Grid>
-                  <Grid item xs={12}>
-                    <TextField
-                      value={ fields.confirmPassword }
-                      onChange={ setFields }
-                      variant="outlined"
-                      required
-                      fullWidth
-                      name="confirmPassword"
-                      label="Confirm password"
-                      type="password"
-                      id="confirmPassword"
-                      autoComplete="current-password"
-
-                      inputRef={register({ required: true, minLength: 8, pattern: /(?=^.{8,}$)((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, 
-                                          validate: confirmPassword => confirmPassword === fields.password})}
-                      error={errors.confirmPassword ? true : false}
-                      helperText={errors.confirmPassword ? 
-                                  errors.confirmPassword?.type === "validate" ? "Both must be the same" : "Password is required (must have 8 digits and...)"
-                                  : ""}
-                  />
-                  </Grid>
-                </Grid>
+                <AddAccountForm
+                  fields={fields}
+                  setFields={setFields}
+                  register={register}
+                  errors={errors}
+                />
                 <Collapse in={openAlert}>
                   <Alert severity="warning" action={
                         <IconButton aria-label="close" color="inherit" size="small" onClick={() => { setOpenAlert(false); }}>
