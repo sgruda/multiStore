0c9f8fbfa42e8667329490dd1c249177b6940773
sgruda <szymon.gruda98@gmail.com>
2020-11-24 20:30:05 +0100

    [FRONTEND] Internationalization of NotFound.js

--- app/frontend/src/pages/NotFound.js
+++ app/frontend/src/pages/NotFound.js
@@ -1,9 +1,11 @@
-import React from "react"; 
+import React from "react";
+import { useTranslation } from 'react-i18next';
  
 function NotFound() { 
+  const { t } = useTranslation();
   return ( 
     <div> 
-      <h3>Sorry, page not found!</h3> 
+      <h3>{t('error.http.404')}</h3> 
     </div> 
   ); 
 } 
