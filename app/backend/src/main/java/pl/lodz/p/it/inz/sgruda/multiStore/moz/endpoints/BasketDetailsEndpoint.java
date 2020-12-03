ff8744c61eeb5b0ffc7e62f8360136383eaf5c4e
sgruda <szymon.gruda98@gmail.com>
2020-12-03 18:05:11 +0100

    [BACKEND] Add adding and removing items to/from basket

--- app/backend/src/main/java/pl/lodz/p/it/inz/sgruda/multiStore/moz/endpoints/BasketDetailsEndpoint.java
+++ app/backend/src/main/java/pl/lodz/p/it/inz/sgruda/multiStore/moz/endpoints/BasketDetailsEndpoint.java
@@ -8,18 +8,25 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import pl.lodz.p.it.inz.sgruda.multiStore.dto.mappers.moz.BasketMapper;
 import pl.lodz.p.it.inz.sgruda.multiStore.dto.moz.BasketDTO;
+import pl.lodz.p.it.inz.sgruda.multiStore.dto.moz.OrderedItemDTO;
 import pl.lodz.p.it.inz.sgruda.multiStore.entities.moz.BasketEntity;
+import pl.lodz.p.it.inz.sgruda.multiStore.entities.moz.OrderedItemEntity;
 import pl.lodz.p.it.inz.sgruda.multiStore.exceptions.AppBaseException;
+import pl.lodz.p.it.inz.sgruda.multiStore.exceptions.http.UnauthorizedRequestException;
 import pl.lodz.p.it.inz.sgruda.multiStore.moz.services.interfaces.BasketDetailsService;
+import pl.lodz.p.it.inz.sgruda.multiStore.moz.services.interfaces.BasketHandlerService;
 import pl.lodz.p.it.inz.sgruda.multiStore.responses.ApiResponse;
 import pl.lodz.p.it.inz.sgruda.multiStore.security.CurrentUser;
 import pl.lodz.p.it.inz.sgruda.multiStore.security.UserPrincipal;
+import pl.lodz.p.it.inz.sgruda.multiStore.utils.components.moz.CheckerMozDTO;
 import pl.lodz.p.it.inz.sgruda.multiStore.utils.components.moz.SignMozDTOUtil;
+
+import javax.validation.Valid;
+import java.util.HashSet;
+import java.util.Set;
 
 
 @Log
@@ -31,12 +38,17 @@ import pl.lodz.p.it.inz.sgruda.multiStore.utils.components.moz.SignMozDTOUtil;
 @RequestMapping("/api/basket")
 public class BasketDetailsEndpoint {
     private BasketDetailsService basketDetailsService;
+    private BasketHandlerService basketHandlerService;
     private SignMozDTOUtil signMozDTOUtil;
+    private CheckerMozDTO checkerMozDTO;
 
     @Autowired
-    public BasketDetailsEndpoint(BasketDetailsService basketDetailsService, SignMozDTOUtil signMozDTOUtil) {
+    public BasketDetailsEndpoint(BasketDetailsService basketDetailsService, BasketHandlerService basketHandlerService,
+                                 SignMozDTOUtil signMozDTOUtil, CheckerMozDTO checkerMozDTO) {
         this.basketDetailsService = basketDetailsService;
+        this.basketHandlerService = basketHandlerService;
         this.signMozDTOUtil = signMozDTOUtil;
+        this.checkerMozDTO = checkerMozDTO;
     }
 
     @GetMapping
@@ -54,5 +66,57 @@ public class BasketDetailsEndpoint {
         BasketDTO basketDTO = basketMapper.toDTO(basketEntity);
         signMozDTOUtil.signBasketDTO(basketDTO);
         return ResponseEntity.ok(basketDTO);
+    }
+
+    @PutMapping("/add")
+    @PreAuthorize("hasRole('ROLE_CLIENT')")
+    public ResponseEntity<?> addToBasket(@Valid @RequestBody BasketDTO basketDTO, @CurrentUser UserPrincipal currentUser) {
+        BasketEntity basketEntity;
+        if(!basketDTO.getOwnerEmail().equals(currentUser.getEmail()))
+            throw new UnauthorizedRequestException();
+        try {
+            checkerMozDTO.checkBasketDTOSignature(basketDTO);
+            basketEntity = basketHandlerService.getBasketEntityByOwnerEmail(currentUser.getEmail());
+            checkerMozDTO.checkBasketDTOVersion(basketEntity, basketDTO);
+            Set<OrderedItemEntity> orderedItemEntitySet = new HashSet<>();
+            for(OrderedItemDTO itemDTO : basketDTO.getOrderedItemDTOS()) {
+                orderedItemEntitySet.add(basketHandlerService.getOrderedItemsEntityOrCreateNew(
+                        itemDTO.getIdentifier(), itemDTO.getOrderedNumber(), itemDTO.getOrderedProduct().getTitle())
+                );
+            }
+            basketEntity.setOrderedItemEntities(orderedItemEntitySet);
+            basketHandlerService.addToBasket(orderedItemEntitySet, basketEntity);
+        } catch (AppBaseException e) {
+            log.severe("Error: " + e);
+            return new ResponseEntity(new ApiResponse(false, e.getMessage()),
+                    HttpStatus.BAD_REQUEST);
+        }
+        return ResponseEntity.ok(new ApiResponse(true, "product.correctly.added.to.basket"));
+    }
+
+    @PutMapping("/remove")
+    @PreAuthorize("hasRole('ROLE_CLIENT')")
+    public ResponseEntity<?> removeFromBasket(@Valid @RequestBody BasketDTO basketDTO, @CurrentUser UserPrincipal currentUser) {
+        BasketEntity basketEntity;
+        if(!basketDTO.getOwnerEmail().equals(currentUser.getEmail()))
+            throw new UnauthorizedRequestException();
+        try {
+            checkerMozDTO.checkBasketDTOSignature(basketDTO);
+            basketEntity = basketHandlerService.getBasketEntityByOwnerEmail(currentUser.getEmail());
+            checkerMozDTO.checkBasketDTOVersion(basketEntity, basketDTO);
+            Set<OrderedItemEntity> orderedItemEntitySet = new HashSet<>();
+            for(OrderedItemDTO itemDTO : basketDTO.getOrderedItemDTOS()) {
+                orderedItemEntitySet.add(basketHandlerService.getOrderedItemsEntityOrCreateNew(
+                        itemDTO.getIdentifier(), itemDTO.getOrderedNumber(), itemDTO.getOrderedProduct().getTitle())
+                );
+            }
+            basketEntity.setOrderedItemEntities(orderedItemEntitySet);
+            basketHandlerService.removeFromBasket(orderedItemEntitySet, basketEntity);
+        } catch (AppBaseException e) {
+            log.severe("Error: " + e);
+            return new ResponseEntity(new ApiResponse(false, e.getMessage()),
+                    HttpStatus.BAD_REQUEST);
+        }
+        return ResponseEntity.ok(new ApiResponse(true, "product.correctly.removed.from.basket"));
     }
 }
---
+++ app/backend/src/main/java/pl/lodz/p/it/inz/sgruda/multiStore/moz/services/implementation/BasketHandlerServiceImpl.java
@@ -0,0 +1,88 @@
+package pl.lodz.p.it.inz.sgruda.multiStore.moz.services.implementation;
+
+import lombok.extern.java.Log;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+import pl.lodz.p.it.inz.sgruda.multiStore.dto.moz.OrderedItemDTO;
+import pl.lodz.p.it.inz.sgruda.multiStore.entities.mop.ProductEntity;
+import pl.lodz.p.it.inz.sgruda.multiStore.entities.moz.BasketEntity;
+import pl.lodz.p.it.inz.sgruda.multiStore.entities.moz.OrderedItemEntity;
+import pl.lodz.p.it.inz.sgruda.multiStore.exceptions.AppBaseException;
+import pl.lodz.p.it.inz.sgruda.multiStore.exceptions.mop.ProductNotExistsException;
+import pl.lodz.p.it.inz.sgruda.multiStore.exceptions.moz.BasketNotExistsException;
+import pl.lodz.p.it.inz.sgruda.multiStore.exceptions.moz.OrderedItemNotExistException;
+import pl.lodz.p.it.inz.sgruda.multiStore.exceptions.moz.ProductAlreadyInactiveException;
+import pl.lodz.p.it.inz.sgruda.multiStore.moz.repositories.BasketRepository;
+import pl.lodz.p.it.inz.sgruda.multiStore.moz.repositories.OrderRepository;
+import pl.lodz.p.it.inz.sgruda.multiStore.moz.repositories.OrderedItemRepository;
+import pl.lodz.p.it.inz.sgruda.multiStore.moz.repositories.ProductRepositoryMOZ;
+import pl.lodz.p.it.inz.sgruda.multiStore.moz.services.interfaces.BasketHandlerService;
+
+import java.util.Optional;
+import java.util.Set;
+
+@Log
+@Service
+@Transactional(
+        isolation = Isolation.READ_COMMITTED,
+        propagation = Propagation.REQUIRES_NEW,
+        transactionManager = "mozTransactionManager",
+        timeout = 5
+)
+public class BasketHandlerServiceImpl implements BasketHandlerService {
+    private BasketRepository basketRepository;
+    private OrderedItemRepository orderedItemRepository;
+    private ProductRepositoryMOZ productRepository;
+
+    @Autowired
+    public BasketHandlerServiceImpl(BasketRepository basketRepository, OrderedItemRepository orderedItemRepository, ProductRepositoryMOZ productRepository) {
+        this.basketRepository = basketRepository;
+        this.orderedItemRepository = orderedItemRepository;
+        this.productRepository = productRepository;
+    }
+
+    @Override
+    @PreAuthorize("hasRole('ROLE_CLIENT')")
+    public BasketEntity getBasketEntityByOwnerEmail(String ownerMail) throws BasketNotExistsException {
+        return basketRepository.findByAccountEntity_Email(ownerMail)
+                .orElseThrow(() -> new BasketNotExistsException());
+    }
+
+    @Override
+    @PreAuthorize("hasRole('ROLE_CLIENT')")
+    public OrderedItemEntity getOrderedItemsEntityOrCreateNew(String identifier, int orderedNumber, String productTitle) throws AppBaseException {
+        Optional<OrderedItemEntity> optional = orderedItemRepository.findByIdentifier(identifier);
+        if(optional.isPresent())
+            return optional.get();
+        else {
+            OrderedItemEntity orderedItemEntity = new OrderedItemEntity();
+            orderedItemEntity.setOrderedNumber(orderedNumber);
+            ProductEntity productEntity = productRepository.findByTitle(productTitle)
+                    .orElseThrow(() -> new ProductNotExistsException());
+            if(!productEntity.isActive())
+                throw new ProductAlreadyInactiveException();
+            orderedItemEntity.setProductEntity(productEntity);
+            orderedItemRepository.saveAndFlush(orderedItemEntity);
+            return orderedItemEntity;
+        }
+    }
+
+    @Override
+    @PreAuthorize("hasRole('ROLE_CLIENT')")
+    public void addToBasket(Set<OrderedItemEntity> orderedItemEntities, BasketEntity basketEntity) {
+        basketEntity.getOrderedItemEntities().addAll(orderedItemEntities);
+        basketRepository.saveAndFlush(basketEntity);
+    }
+
+    @Override
+    @PreAuthorize("hasRole('ROLE_CLIENT')")
+    public void removeFromBasket(Set<OrderedItemEntity> orderedItemEntities, BasketEntity basketEntity) {
+        basketEntity.getOrderedItemEntities()
+                .removeIf(item -> !orderedItemEntities.contains(item));
+        basketRepository.saveAndFlush(basketEntity);
+    }
+}
