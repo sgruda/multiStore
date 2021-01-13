9efa1b4c40895abf362f9cd53366a463b887e945
sgruda <szymon.gruda98@gmail.com>
2021-01-13 18:05:14 +0100

    [FRONTEND] Fix error messege after failed authentications

--- app/frontend/src/pages/SignIn.js
+++ app/frontend/src/pages/SignIn.js
@@ -78,6 +78,7 @@ function SignIn() {
 
 
   const convertValidationMessage = (message) => {
+    if(message.startsWith("error")) return t(message);
     let retMessage = '';
     message = message.replace('{', '').replace('}', '')
     let parts = message.split(", ");
